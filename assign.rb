89486eac279ca30cfa22852da025db9807a0858c
reymond107 <rey@DESKTOP-DSHCCU3.localdomain>
2020-10-31 02:33:41 +0800

    added assign.rb about inheritance

---
+++ assign.rb
@@ -0,0 +1,31 @@
+# We have a "Confection" class representing baked desserts. Your job is to create a new subclass of "Confection", named "Cupcake" and "Banana Cake". Cupcakes and Banana Cakes need to be baked just like other confections, but cupcakes also need to be frosted afterward. Write a "prepare" method for your "Cupcake" and "Banana Cake" class that prints "Baking at 350 degrees for 25 minutes.", and then prints "Applying frosting" for "Cupcake" only. #
+
+
+class Confection
+    def prepare
+        puts "Baking at 350 degrees for 25 minutes." 
+    end
+end
+
+module Frosting
+    def frosting
+        puts "Applying frosting"
+    end
+end
+
+class Cupcakes < Confection
+    include Frosting
+end
+
+class Banana_Cake < Confection
+end
+
+
+
+
+cup = Cupcakes.new
+cup.prepare
+cup.frosting
+
+banana = Banana_Cake.new
+banana.prepare
