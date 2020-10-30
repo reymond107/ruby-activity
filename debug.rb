0a2c2321a5c974147e381b09b56cfbd74429b7af
reymond107 <67710867+reymond107@users.noreply.github.com>
2020-10-30 22:27:34 +0800

    Update debug.rb
    updated code

--- debug.rb
+++ debug.rb
@@ -1,26 +1,11 @@
 class Profile
-  attr_accessor :full_name
-  attr_accessor :age
-  attr_accessor :address
-  attr_accessor :work
+  attr_accessor :full_name, :age, :address, :work
 
   def initialize(full_name, age, address, work)
     @full_name = full_name
     @age = age
     @address = address
     @work = work
-  end
-  def full_name
-    @full_name
-  end
-  def age
-    @age
-  end
-  def address
-    @address
-  end
-  def work
-    @work
   end
 end
 
