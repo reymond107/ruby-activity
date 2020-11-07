73da38ae231b5e01a01d5793741e4b7b3c1d249c
reymond107 <rey@DESKTOP-DSHCCU3.localdomain>
2020-11-07 09:50:12 +0800

    added activity 2 in practice.sql

--- practice.sql
+++ practice.sql
@@ -1,21 +1,23 @@
+-- Activity 1
+
 CREATE TABLE students (id integer, first_name type VARCHAR(20), middle_name type VARCHAR(20),last_name type VARCHAR(20), age integer, location_address type VARCHAR(20));
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(1, 'Juan', 'Blank', 'Cruz', 18, 'Manila')
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(2, 'John', 'Blank', 'Young', 20, 'Manila')
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(3, 'Victor', 'Blank', 'Rivera', 21, 'Manila')
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(4, 'Adrian', 'Blank', 'Co', 19, 'Laguna')
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(5, 'Pau', 'Blank', 'Riosa', 22, 'Marikina')
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+INSERT INTO students (id, first_name, middle_name, last_name, age, location_address) 
 VALUES(6, 'Piolo', 'Blank', 'Pascual', 25, 'Manila')
 
 UPDATE students
@@ -24,3 +26,20 @@ WHERE id = 1;
 
 DELETE FROM students
 WHERE first_name = 'Piolo'
+
+
+-- Activity 2
+
+SELECT COUNT(id)
+FROM students;
+
+SELECT location_address
+FROM students
+WHERE location_address= 'Manila';
+
+SELECT AVG(age)
+FROM students
+
+SELECT age
+FROM students
+ORDER BY age DESC;
